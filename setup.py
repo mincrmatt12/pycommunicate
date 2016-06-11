ab6cffa12b15748a2390716c8a8f00f653e2ece7
mincrmatt12 <matthewmirvish@hotmail.com>
2016-06-11 09:28:38 -0400

    license + setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+
+def get_markdown():
+    with open('README.md') as f:
+        return f.read()
+
+
+requires = '''click
+eventlet
+Flask
+Flask-SocketIO
+greenlet
+itsdangerous
+Jinja2
+MarkupSafe
+python-engineio
+python-socketio
+six
+Werkzeug
+wheel'''.splitlines()
+
+
+setup(
+    name='pycommunicate',
+    version='0.0.5',
+    description='A web library focusing on handling JS events server-side',
+    long_description=get_markdown(),
+    url='https://github.com/mincrmatt12/pycommunicate',
+    author='mincrmatt12',
+    author_email='nope@nope.com',
+    license='MIT',
+    packages=['pycommunicate'],
+    zip_safe=False,
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    ]
+)
